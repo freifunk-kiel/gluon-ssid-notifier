b898e2d4abc4fb77acb99ed8755040fc059b4a71
rubo77 <github@r.z11.de>
2016-08-04 01:30:29 +0200

    Release 0.1: start/stop Offline SSID without wifi restart
    successfully tested on WR841N v8

--- files/lib/gluon/ssid-notifier/ssid-notifier.sh
+++ files/lib/gluon/ssid-notifier/ssid-notifier.sh
@@ -1,31 +1,39 @@
 #!/bin/sh
 
-# At first some Definitions:
-
-ONLINE_SSID=$(uci get wireless.client_radio0.ssid -q)
-: ${ONLINE_SSID:=FREIFUNK}   # if for whatever reason ONLINE_SSID is NULL
-OFFLINE_PREFIX='FF kein Netz:' # Use something short to leave space for the nodename
+OFFLINE_PREFIX='FF kein Netz:' # use something short to leave space for the nodename (no "/" allowed!)
 
 UPPER_LIMIT='55' # Above this limit the offline SSID will be off
 LOWER_LIMIT='45' # Below this limit the offline SSID will be on
-# In-between these two values the SSID will never be changed to preven it from toggeling every Minute.
+# In-between these two values the SSID will never be changed. prevents from toggeling every cron call
 
-# Generate an Offline SSID with the first and last Part of the nodename to allow owner to recognise wich node is down
-NODENAME=`uname -n`
-if [ ${#NODENAME} -gt $((30 - ${#OFFLINE_PREFIX})) ] ; then # 32 would be possible as well
-	HALF=$(( (28 - ${#OFFLINE_PREFIX} ) / 2 )) # calculate the length of the first part of the node identifier in the offline-ssid
-	SKIP=$(( ${#NODENAME} - $HALF )) # jump to this charakter for the last part of the name
-	OFFLINE_SSID="$OFFLINE_PREFIX${NODENAME:0:$HALF}..${NODENAME:$SKIP:${#NODENAME}}" # use the first and last part of the nodename for nodes with long name
-else
-	OFFLINE_SSID="$OFFLINE_PREFIX$NODENAME" # great! we are able to use the full nodename in the offline ssid
-fi
-
-#is there an active gateway?
-GATEWAY_TQ=$(batctl gwl | grep "^=>" | awk -F '[()]' '{print $2}' | tr -d " ") # grep the connection quality of the currently used gateway
-
+HOSTAPD=/var/run/hostapd-phy0.conf
 OI=wireless.client_radio0_offline
 
+GWL="$(batctl gwl)"
+if [ "$?" == "1" ]; then 
+	echo "wifi button off?"
+	exit
+fi
+
+TQ=$(echo "$GWL" | grep "^=>" | awk -F '[()]' '{print $2}' | tr -d " ") # connection quality of the currently used gateway
+if [ ! $TQ ]; then
+	echo "TQ: '$TQ', wifi still running? do nothing"
+	exit
+elif [ $TQ -ge ${LOWER_LIMIT} -a $TQ -le ${UPPER_LIMIT} ]; then 
+	# this is just get a clean run if we are in-between the grace periode
+	echo "TQ: $TQ, this is between ${LOWER_LIMIT} and ${UPPER_LIMIT}; do nothing"
+	exit
+fi
+
 IF_EXISTS=$(uci get $OI -q)
+NODENAME=`uname -n`
+if [ ${#NODENAME} -gt $((32 - ${#OFFLINE_PREFIX})) ] ; then
+	HALF=$(( (30 - ${#OFFLINE_PREFIX} ) / 2 ))
+	SKIP=$(( ${#NODENAME} - $HALF )) # jump to this character for the last part of the name
+	OFFLINE_SSID="${OFFLINE_PREFIX}${NODENAME:0:$HALF}..${NODENAME:$SKIP:${#NODENAME}}" # start .. end
+else
+	OFFLINE_SSID="${OFFLINE_PREFIX}${NODENAME}" # full nodename
+fi
 
 if [ ! $IF_EXISTS ]; then
 	uci set $OI=wifi-iface
@@ -34,38 +42,55 @@ if [ ! $IF_EXISTS ]; then
 	uci set $OI.mode='ap'
 	uci set $OI.disabled='1'
 	uci set $OI.ssid="${OFFLINE_SSID}"
+	uci commit $OI; wifi; exit
+elif [ "$(uci get $OI.ssid -q)" != "${OFFLINE_SSID}" ]; then
+	#hostname changed
+	uci set $OI.ssid="${OFFLINE_SSID}"
+	uci commit $OI; wifi; exit
 fi
 
-DISABLED=$(uci get $OI.disabled -q)
-HUP_NEEDED=0
+OI_IS_DISABLED=$(uci get $OI.disabled -q)
+LINE_IN_HOSTAPD=$(grep -n "ssid=${OFFLINE_SSID}" ${HOSTAPD} | cut -d':' -f1)
+if [ "${LINE_IN_HOSTAPD}" == "" ]; then
+	echo "the offline SSID is not in hostapd file"
+	OI_IS_DISABLED=1
+elif [ "$(head -n $LINE_IN_HOSTAPD $HOSTAPD | tail -n 11|grep "ignore_broadcast_ssid=1")" != "" ]; then
+	echo "the offline SSID is in hostapd file but disabled there"
+	OI_IS_DISABLED=1
+fi
 
-if [ ! $GATEWAY_TQ ]; then
-	echo "TQ: '$GATEWAY_TQ', wifi still running? do nothing"
-elif [ $GATEWAY_TQ -ge $LOWER_LIMIT -a $GATEWAY_TQ -le $UPPER_LIMIT ]; then 
-	# this is just get a clean run if we are in-between the grace periode
-	echo "TQ: $GATEWAY_TQ, this is between $LOWER_LIMIT and $UPPER_LIMIT; do nothing"
-elif [ $GATEWAY_TQ -gt $UPPER_LIMIT ]; then
-	if [ $DISABLED == 1 ]; then
-		echo "Gateway TQ: $GATEWAY_TQ; node is still online"
+HUP_NEEDED=0
+LINE=$(grep -n "wlan0-" ${HOSTAPD} | cut -d':' -f1)
+if [ $TQ -gt $UPPER_LIMIT ]; then
+	if [ $OI_IS_DISABLED == 1 ]; then
+		echo "Gateway TQ: $TQ; node is still online"
 	else
-		logger -s -t "ssid-notifier" -p 5 "Gateway TQ is $GATEWAY_TQ node changed to online"
-		uci set $OI.disabled='1'
-		uci commit $OI
-		HUP_NEEDED=1
+		logger -s -t "ssid-notifier" -p 5 "node is online; Gateway TQ: $TQ; disable offline-SSID"
+		if [ "${LINE_IN_HOSTAPD}" == "" ]; then
+			uci set $OI.disabled='1'
+			uci commit $OI; wifi; exit
+		else
+			echo "$(awk -v RS= -v ORS='\n\n' '/ssid='"${OFFLINE_SSID}"'/{sub(/ignore_broadcast_ssid=0/,"ignore_broadcast_ssid=1")} 1' $HOSTAPD)" > $HOSTAPD
+			HUP_NEEDED=1
+		fi
 	fi
 else
-	if [ $DISABLED == 0 ]; then
-		echo "Gateway TQ: $GATEWAY_TQ; node is still considered offline"
+	if [ $OI_IS_DISABLED == 0 ]; then
+		echo "Gateway TQ: $TQ; node is still considered offline"
 	else
-		logger -s -t "ssid-notifier" -p 5 "Gateway TQ is $GATEWAY_TQ node is considered offline"
-		uci set $OI.disabled='0'
-		uci commit $OI
-		HUP_NEEDED=1
+		logger -s -t "ssid-notifier" -p 5 "node is offline; Gateway TQ: $TQ; enable offline-SSID"
+		if [ "${LINE_IN_HOSTAPD}" == "" ]; then
+			uci set $OI.disabled='0'
+			uci commit $OI; wifi; exit
+		else
+			echo "$(awk -v RS= -v ORS='\n\n' '/ssid='"${OFFLINE_SSID}"'/{sub(/ignore_broadcast_ssid=1/,"ignore_broadcast_ssid=0")} 1' $HOSTAPD)" > $HOSTAPD
+			HUP_NEEDED=1
+		fi
 	fi
 fi
 
 if [ $HUP_NEEDED == 1 ]; then
-	wifi
+	killall -SIGHUP hostapd # Send HUP to all hostapd um die neue SSID zu laden
 	HUP_NEEDED=0
-	echo "HUP!"
+	echo "SIGHUP!"
 fi
